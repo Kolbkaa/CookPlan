3a29fd74c93029bf96b6df78684eca083d92682f
Robot CodersLab <travis-ci@coderslab.pl>
2020-03-10 09:58:12 +0100

    Initial commit

---
+++ src/PlanFood/PlanFood.Mvc/Context/PlanFoodContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PlanFood.Mvc.Models;
+using PlanFood.Mvc.Models.Db;
+
+namespace PlanFood.Mvc.Context
+{
+	public class PlanFoodContext : DbContext
+	{
+		public PlanFoodContext(DbContextOptions<PlanFoodContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Book> Books { get; set; }
+	}
+}
---
+++ src/PlanFood/PlanFood.Mvc/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PlanFood.Mvc.Context;
+using PlanFood.Mvc.Services;
+using PlanFood.Mvc.Services.Interfaces;
+
+namespace PlanFood.Mvc
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<PlanFoodContext>(builer => builer.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=PlanFood;Integrated Security=True"));
+
+			services.AddScoped<IBookService, BookService>();
+
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action}/{id?}");
+			});
+		}
+	}
+}
