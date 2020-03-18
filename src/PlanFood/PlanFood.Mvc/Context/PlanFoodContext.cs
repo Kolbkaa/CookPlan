600a796cf77656c178d1c0ddbaa38ec8a0aff316
asiutra <adamsiutra@onet.pl>
2020-03-18 12:38:25 +0100

    ImplementPlanCRUD

--- src/PlanFood/PlanFood.Mvc/Context/PlanFoodContext.cs
+++ src/PlanFood/PlanFood.Mvc/Context/PlanFoodContext.cs
@@ -1,21 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
- using Microsoft.EntityFrameworkCore;
- using PlanFood.Mvc.Models.Db;
+using Microsoft.EntityFrameworkCore;
+using PlanFood.Mvc.Models.Db;
 
 
- namespace PlanFood.Mvc.Context
+namespace PlanFood.Mvc.Context
 {
-	public class PlanFoodContext : IdentityDbContext<User,IdentityRole<int>,int>
-	{
-		public PlanFoodContext(DbContextOptions<PlanFoodContext> options) : base(options)
-		{
-		}
+    public class PlanFoodContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public PlanFoodContext(DbContextOptions<PlanFoodContext> options) : base(options)
+        {
+        }
 
-		public DbSet<Book> Books { get; set; }
-		public DbSet<Recipe> Recipes { get; set; }
-    public DbSet<RecipePlans> RecipePlans { get; set; }
-    public DbSet<DayName> DayNames { get; set; }
-
-	}
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipePlans> RecipePlans { get; set; }
+        public DbSet<DayName> DayNames { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+    }
 }
--- src/PlanFood/PlanFood.Mvc/Models/Db/Plan.cs
+++ src/PlanFood/PlanFood.Mvc/Models/Db/Plan.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,15 @@ namespace PlanFood.Mvc.Models.Db
 {
     public class Plan
     {
+        public int Id { get; set; }
+        [Required, Column(TypeName = "datetime2(7)")]
+        public DateTime Created { get; set; }
+        [MaxLength]
+        public string Description { get; set; }
+        [StringLength(45)]
+        public string Name { get; set; }
+        [Required, StringLength(450)]
+        public int? UserId { get; set; }
+        public User User { get; set; }
     }
 }
