b4dce07f96a707b4b6ccf40710f8979cb7421623
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2020-03-19 18:36:22 +0100

    Added recipe view and controller

---
+++ src/PlanFood/PlanFood.Mvc/Controllers/RecipeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PlanFood.Mvc.Models.ViewModels;
+
+namespace PlanFood.Mvc.Controllers
+{
+    //[Authorize]
+    public class RecipeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(AddRecipeViewModel model)
+        {
+            return RedirectToAction("List", "Recipe");
+        }
+    }
+}
---
+++ src/PlanFood/PlanFood.Mvc/Models/ViewModels/AddRecipeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanFood.Mvc.Models.ViewModels
+{
+    public class AddRecipeViewModel
+    {
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Name { get; set; }
+        [MaxLength(ErrorMessage = "Za dużo znaków")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Ingredients { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public int PreparationTime { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        
+        public string Preparation { get; set; }
+	}
+}
