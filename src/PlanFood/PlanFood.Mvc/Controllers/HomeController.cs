4befcdcb69dea7c27782bc7d9ebc4b83ff1d0ad1
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2020-03-19 22:41:16 +0100

    Added method to homeController and view.

--- src/PlanFood/PlanFood.Mvc/Controllers/HomeController.cs
+++ src/PlanFood/PlanFood.Mvc/Controllers/HomeController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PlanFood.Mvc.Services.Interfaces;
 
 namespace PlanFood.Mvc.Controllers
 {
-	public class HomeController : Controller
-	{
-		public IActionResult Index()
-		{
-		    return View();
-		}
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         public IActionResult Contact()
         {
@@ -22,5 +23,11 @@ namespace PlanFood.Mvc.Controllers
         {
             return View();
         }
-	}
+
+        public async Task<IActionResult> Recipe([FromServices] IRecipeService recipeService)
+        {
+            var recipeList = await recipeService.GetAllAsync();
+            return View(recipeList);
+        }
+    }
 }
