ea9a3b30ba50a2c760deae920729c77327eb555a
Tomek <kemot15@tlen.pl>
2020-03-19 11:47:37 +0100

    dodanie brakujacej klamry

--- src/PlanFood/PlanFood.Mvc/Context/PlanFoodContext.cs
+++ src/PlanFood/PlanFood.Mvc/Context/PlanFoodContext.cs
@@ -6,7 +6,7 @@ using PlanFood.Mvc.Models.Db;
 
 namespace PlanFood.Mvc.Context
 {
-	public class PlanFoodContext : IdentityDbContext<User,IdentityRole<int>,int>
+	public class PlanFoodContext : IdentityDbContext<User, IdentityRole<int>, int>
 	{
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
@@ -17,11 +17,12 @@ namespace PlanFood.Mvc.Context
 		}
 		public PlanFoodContext(DbContextOptions<PlanFoodContext> options) : base(options)
 		{
-		}		
+		}
 
 		public DbSet<Book> Books { get; set; }
 		public DbSet<Recipe> Recipes { get; set; }
 		public DbSet<RecipePlans> RecipePlans { get; set; }
 		public DbSet<DayName> DayNames { get; set; }
-    public DbSet<Plan> Plans { get; set; }
+		public DbSet<Plan> Plans { get; set; }
 	}
+}
